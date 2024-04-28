709f2a5cdd725bef6e604374802c022040904e8d
Selim <70847894+slm0128@users.noreply.github.com>
2024-04-28 18:49:09 +0200

    Add files via upload

---
+++ CarClicker/src/Player.java
@@ -0,0 +1,15 @@
+public class Player{
+
+    private String name;
+
+         Player(String name){
+        this.name=name;
+        
+
+    }
+
+    public String getName() {
+        return name;
+    }
+    
+}
