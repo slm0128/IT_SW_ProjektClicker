66f387206104993498ebb097d622ced1282bf948
Selim <70847894+slm0128@users.noreply.github.com>
2024-05-20 16:51:45 +0200

    Delete GUI/test directory

--- GUI/test/src/Color.java
+++
@@ -1,8 +0,0 @@
-import javax.swing.JColorChooser;
-
-public class Color {
-    public static void main(String[] args) throws Exception {
-        java.awt.Color ausgColor= JColorChooser.showDialog(null, "Farbeauswahl", null);     //showDialog zeigt Vorgefertigte Fenster/klassen und ist static
-        System.out.println(ausgColor);
-    }
-}
